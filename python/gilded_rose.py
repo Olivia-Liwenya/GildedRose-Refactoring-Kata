fe3cc74bc8a7e7493463c32d36429f05229e208f
Olivia-Liwenya <li_wenya_naya@163.com>
2023-08-10 22:49:14 +0800

    change update rules

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -4,36 +4,97 @@ class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
+        self.general_quality_limit = 50
+        self.special_quality_limit = 80
+
+    def quality_limit(self, item):
+        if item.sell_in == 0:
+            return 0
+        return min(item.quality, self.general_quality_limit)
+    
+    def sell_in_limit(self, item):
+        return max(item.quality, 0)
+    
+
+    def Aged_change(self, item):
+
+        if item.sell_in > 10:
+            item.quality += 1
+
+        elif item.sell_in > 5:
+            item.quality += 2
+
+        elif item.quality > 0:
+            item.quality += 3
+
+        else:
+            item.quality = 0
+
+        item.quality = self.quality_limit(item)
+
+        item.sell_in = item.sell_in - 1
+        item.sell_in = self.sell_in_limit(item)
+
+
+    def Backstage_change(self, item):
+        if item.sell_in > 10:
+            item.quality += 1
+
+        elif item.sell_in > 5:
+            item.quality += 2
+
+        elif item.quality > 0:
+            item.quality += 3
+
+        else:
+            item.quality = 0
+
+        item.quality = self.quality_limit(item)
+
+        item.sell_in = item.sell_in - 1
+        item.sell_in = self.sell_in_limit(item)
+
+    def Sulfuras_change(self, item):
+        item.quality = self.special_quality_limit
+        item.sell_in = item.sell_in - 1
+        item.sell_in = self.sell_in_limit(item)
+
+    def Conjured_change(self, item):
+        item.quality = item.quality - 2
+        item.quality = self.quality_limit(item)
+
+        item.sell_in = item.sell_in - 1
+        item.sell_in = self.sell_in_limit(item)
+
+
+    def General_change(self, item):
+        item.quality = item.quality - 1
+        item.quality = self.quality_limit(item)
+        item.sell_in = item.sell_in - 1
+        item.sell_in = self.sell_in_limit(item)
+
+    
+
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            if item.name == 'Aged Brie':
+                self.Aged_change(item)
+
+            elif item.name == 'Backstage passes':
+                self.Backstage_change(item)
+            
+            elif item.name == 'Sulfuras':
+                self.Sulfuras_change(item)
+            
+            elif item.name == 'Conjured':
+                self.Conjured_change(item)
+            
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                self.General_change(item)
+            
+            
+        
 
 
 class Item:
